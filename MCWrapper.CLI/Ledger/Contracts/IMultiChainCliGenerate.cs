5f47fd37323705beb2b336d2fa2a0da8d65c610b
scanlifter <ryan@ryangoodwin.dev>
2019-11-23 14:52:13 -0700

    IMultiChainCliGenerate Code Comments update

--- MCWrapper.CLI/Ledger/Contracts/IMultiChainCliGenerate.cs
+++ MCWrapper.CLI/Ledger/Contracts/IMultiChainCliGenerate.cs
@@ -13,11 +13,75 @@ namespace MCWrapper.CLI.Ledger.Clients
     /// </summary>
     public interface IMultiChainCliGenerate : IMultiChainCli
     {
+        /// <summary>
+        /// 
+        /// <para>Return if the server is set to generate coins or not. The default is false.</para>
+        /// <para>It is set with the command line argument -gen (or bitcoin.conf setting gen)</para>
+        /// <para>It can also be set with the setgenerate call.</para>
+        /// <para>Blockchain name is inferred from CliOptions properties.</para>
+        ///
+        /// </summary>
+        /// <returns>If the server is set to generate coins or not</returns>
         Task<CliResponse<bool>> GetGeneratedAsync();
+
+        /// <summary>
+        /// 
+        /// <para>Return if the server is set to generate coins or not. The default is false.</para>
+        /// <para>It is set with the command line argument -gen (or bitcoin.conf setting gen)</para>
+        /// <para>It can also be set with the setgenerate call.</para>
+        /// <para>Blockchain name is explicitly passed as parameter.</para>
+        ///
+        /// </summary>
+        /// <param name="blockchainName">Name of target blockchain</param>
+        /// <returns>If the server is set to generate coins or not</returns>
         Task<CliResponse<bool>> GetGeneratedAsync(string blockchainName);
+
+        /// <summary>
+        /// 
+        /// <para>Returns a recent hashes per second performance measurement while generating.</para>
+        /// <para>See the getgenerate and setgenerate calls to turn generation on and off.</para>
+        /// <para>Blockchain name is inferred from CliOptions properties.</para>
+        ///
+        /// </summary>
+        /// <returns>(numeric) The recent hashes per second when generation is on (will return 0 if generation is off)</returns>
         Task<CliResponse<int>> GetHashesPerSecAsync();
+
+        /// <summary>
+        /// 
+        /// <para>Returns a recent hashes per second performance measurement while generating.</para>
+        /// <para>See the getgenerate and setgenerate calls to turn generation on and off.</para>
+        /// <para>Blockchain name is explicitly passed as parameter.</para>
+        ///
+        /// </summary>
+        /// <param name="blockchainName">Name of target blockchain</param>
+        /// <returns>(numeric) The recent hashes per second when generation is on (will return 0 if generation is off)</returns>
         Task<CliResponse<int>> GetHashesPerSecAsync(string blockchainName);
+
+        /// <summary>
+        /// 
+        /// <para>Set 'generate' true or false to turn generation on or off.</para>
+        /// <para>Generation is limited to 'genproclimit' processors, -1 is unlimited.</para>
+        /// <para>See the getgenerate call for the current setting.</para>
+        /// <para>Blockchain name is inferred from CliOptions properties.</para>
+        ///
+        /// </summary>
+        /// <param name="generate">Set to true to turn on generation, off to turn off.</param>
+        /// <param name="gen_proc_limit">Set the processor limit for when generation is on. Can be -1 for unlimited.</param>
+        /// <returns>String value identifying this transaction</returns>
         Task<CliResponse<object>> SetGenerateAsync(bool generate, int gen_proc_limit);
+
+        /// <summary>
+        /// 
+        /// <para>Set 'generate' true or false to turn generation on or off.</para>
+        /// <para>Generation is limited to 'genproclimit' processors, -1 is unlimited.</para>
+        /// <para>See the getgenerate call for the current setting.</para>
+        /// <para>Blockchain name is explicitly passed as parameter.</para>
+        ///
+        /// </summary>
+        /// <param name="blockchainName">Name of target blockchain</param>
+        /// <param name="generate">Set to true to turn on generation, off to turn off.</param>
+        /// <param name="gen_proc_limit">Set the processor limit for when generation is on. Can be -1 for unlimited.</param>
+        /// <returns>String value identifying this transaction</returns>
         Task<CliResponse<object>> SetGenerateAsync(string blockchainName, bool generate, int gen_proc_limit);
     }
 }
